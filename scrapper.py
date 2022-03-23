2b2ce2e6b2d5feef1a346e38e31c8bb284bd74bf
tanynova99 <balabolka2000@gmail.com>
2022-03-23 12:38:54 +0300

    checks parser

--- scrapper.py
+++ scrapper.py
@@ -172,10 +172,10 @@ class HTMLParser:
         for author in authors:
             self.article.author.append(author)
 
-        date_raw = article_bs.find(class_="page__title").text()
+        date_raw = article_bs.find("h1", class_="page__title").text()
 
         # Only year is available, the № of issues per year doesn't correspond with months
-        if date_raw:
+        if date_raw != "":
             self.article.date = datetime.datetime.strptime(date_raw[:4], '%Y')
 
 
