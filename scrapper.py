d75437708617ffd186beb037ed80640d520486be
tanynova99 <balabolka2000@gmail.com>
2022-03-23 21:58:28 +0300

    parser test

--- scrapper.py
+++ scrapper.py
@@ -167,8 +167,8 @@ class HTMLParser:
         """
         self.article.title = article_bs.find(class_="article_title")
 
-        authors = article_bs.find_all("a", class_="link link_const article__author")
-        self.article.author = authors("q")
+        authors = article_bs.find("a", class_="link link_const article__author")
+        self.article.author = authors.text
 
         date_raw = re.search(r"\d{4} Выпуск", article_bs.text)
 
