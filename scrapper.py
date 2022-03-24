e79f2ef9375f2194d650299aa9f532588db64100
tanynova99 <balabolka2000@gmail.com>
2022-03-24 12:25:49 +0300

    parser test

--- scrapper.py
+++ scrapper.py
@@ -147,15 +147,19 @@ class HTMLParser:
         """
         Scrap the text from PDF link embedded in article url
         """
-        possible_pdfs = article_bs.find_all("a", class_="article-panel__item button-icon")[1]
+        possible_pdfs = article_bs.find_all("a", class_="article-panel__item button-icon")
 
-        pdf_raw = PDFRawFile(DOMAIN + possible_pdfs["href"], self.article_id)
+        for pdf in possible_pdfs:
+            if ".pdf" in pdf["href"]:
+                pdf_raw = PDFRawFile(DOMAIN + possible_pdfs["href"], self.article_id)
 
-        pdf_raw.download()
-        pdf_text = pdf_raw.get_text()
+                pdf_raw.download()
+                pdf_text = pdf_raw.get_text()
 
-        pdf_text = pdf_text.split("Список литературы")
-        self.article.text = pdf_text[0]
+                pdf_text = pdf_text.split("Список литературы")
+                self.article.text = pdf_text[0]
+
+                break
 
 
     def _fill_article_with_meta_information(self, article_bs):
