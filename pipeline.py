adbc4b9bb86830d6ee83ff7419e534f179e7d210
tanynova99 <balabolka2000@gmail.com>
2022-04-13 01:13:58 +0300

    outline of validate func

--- pipeline.py
+++ pipeline.py
@@ -2,6 +2,10 @@
 Pipeline for text processing implementation
 """
 
+from pathlib import Path
+
+from constants import ASSETS_PATH
+
 
 class EmptyDirectoryError(Exception):
     """
@@ -9,9 +13,20 @@ class EmptyDirectoryError(Exception):
     """
 
 
+class NotDirectoryError(Exception):
+    """
+    Not a directory
+    """
+
+
 class InconsistentDatasetError(Exception):
     """
     Corrupt data: numeration is expected to start from 1 and to be continuous
+    """
+
+
+class FileNotFoundError(Exception):
+    """
     """
 
 
@@ -88,12 +103,27 @@ def validate_dataset(path_to_validate):
     """
     Validates folder with assets
     """
-    pass
+
+    if not path_to_validate.is_dir():
+        raise NotADirectoryError
+
+    if not any(Path(path_to_validate).iterdir()):
+        raise EmptyDirectoryError
+
+    file_formats = [".json", ".txt", ".pdf"]
+    files = path_to_validate.iterdir()
+    for i, file in enumerate(files, 1):
+        if str(i) != str(file.name)[0]:
+            raise InconsistentDatasetError
+        if file.suffix not in file_formats:
+            raise FileNotFoundError
+
+    return None
 
 
 def main():
     # YOUR CODE HERE
-    pass
+    validate_dataset(ASSETS_PATH)
 
 
 if __name__ == "__main__":
